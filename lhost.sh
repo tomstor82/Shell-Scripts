ab79e369db603cc1d1293e6a36bfae2719bcce6b
Tom Storebø <tomstoreboe@gmail.com>
2021-02-11 06:59:28 +0000

    Added jsx support

--- lhost.sh
+++ lhost.sh
@@ -11,7 +11,10 @@ EXAMPLE="Example: 'lhost ~/Documents/index.html'\n\n";
 # ********** if [[ -n $# ]]; then ************* ADD POSSIBILITY TO WORK WITH MORE THAN ONE ARG
 
 if [[ $1 =~ ^.+\.(html||php||jsx||js||css||scss)$ ]]; then
-    echo 4109 | sudo -S cp $1 /var/www/html/index.html;
+	if [[ $1 =~ ^.+\.html$ ]]; then
+		echo 4109 | sudo -S cp $1 /var/www/html/index.html;
+	else echo 4109 | sudo -S cp $1 /var/www/html/;
+	fi;
 # Otherwise notify user
 elif [ -z $1 ]; then
 	printf "\nlhost: Missing file argument -- '1'\n\nRequires argument to copy file to the Apache Server directory.\n\n${EXAMPLE}"
