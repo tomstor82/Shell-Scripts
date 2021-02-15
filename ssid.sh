8f926093b880c5d3d1036267f67b02d68a2643ab
Tom Storebø <65083884+tomstor82@users.noreply.github.com>
2021-02-15 10:50:13 +0000

    Mod: Profile i info break

--- ssid.sh
+++ ssid.sh
@@ -5,8 +5,8 @@
 # If windows machine
 if [[ -f /mnt/c/Windows/System32/netsh.exe ]]; then
 	netsh="/mnt/c/Windows/System32/netsh.exe";
-    ${netsh} wlan show profile | more && read -p "Type Wireless Profile Name: " ssid && ${netsh} wlan show profile ${ssid} key=clear;
+    ${netsh} wlan show profile | more && read -p "Type Wireless Profile Name: " ssid && ${netsh} wlan show profile ${ssid} key=clear | more;
 # Unix like machine
 else
-	echo "Saved Networks:" && echo "" && ls /etc/NetworkManager/system-connections/ | more && echo "" && read -p "Type SSID name for details: " ssid && echo 4109 | sudo -S cat /etc/NetworkManager/system-connections/$ssid && echo "";
+	echo "Saved Networks:" && echo "" && ls /etc/NetworkManager/system-connections/ | more && echo "" && read -p "Type SSID name for details: " ssid && echo 4109 | sudo -S cat /etc/NetworkManager/system-connections/$ssid | more && echo "";
 fi;
