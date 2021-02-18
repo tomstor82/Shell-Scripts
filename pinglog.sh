d736a0991f7a48c1323d1de27db5506012820ba2
Tom Storebø <tomstor@msn.com>
2021-02-18 08:40:37 +0000

    Fix: logging

--- pinglog.sh
+++ pinglog.sh
@@ -134,10 +134,13 @@ function logLines() {
 function pingStats() {
 	while true
 	do
-		nohup ping -qc "$COUNT" "$IP" 2> temp | grep -Poz '(?s)[-]{3}\ ([\d]{1,3}\.){3}[\d]{1,3}\ ping\ statistics\ [-]{3}.[\d]+([.][\d]+)?\ packets.+\ loss' >> $LOGFILE && printf "\n" >> $LOGFILE;
-		#1>> $LOGFILE 2> /dev/null &&\
+		nohup ping -qc "$COUNT" "$IP" 2> temp | grep -Poz '(?s)[-]{3}\ ([\d]{1,3}\.){3}[\d]{1,3}\ ping\ statistics\ [-]{3}.[\d]+([.][\d]+)?\ packets.+\ loss' >> $LOGFILE;
+		# start new line
+		printf "\n" >> $LOGFILE;
+		# add time stamp
 		printf "$(date)\n" >> $LOGFILE;
-		echo "" >> $LOGFILE &&\
+		# add empty line
+		echo "" >> $LOGFILE;
 
 		# delete lines 1 through 9 of the log when exceeding set log size
 		if [[ $(logLines) -gt $LOGSIZE ]]; then
