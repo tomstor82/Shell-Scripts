81bb1b3d2058a4da5b19364544d049eb390a143b
Tom Storebø <tomstor@msn.com>
2021-03-03 07:37:01 +0000

    Added temperature pane

--- tmux-auto.sh
+++ tmux-auto.sh
@@ -9,8 +9,12 @@ start=$(tmux new -s $session \;\
 		split-window 'echo 4109 | sudo -S dmesg -T --follow' \;\
 		select-pane -t 0 \;\
 		select-layout tiled \;\
+		select-pane -t 1 \;\
+		split-window '/home/tom/scripts/temperature.sh' \;\
+		select-pane -t 0 \;\
 		new-window 'ssh pi' \;\
 		select-window -t 0);
+		
 
 		#split-window top \;\
 		#split-window 'tail -f /var/log/syslog' \;\
